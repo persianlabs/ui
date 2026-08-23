962d2ec5387be5117b0d3c82ed5b34a25280e55a
taymakz <taymazak1382@gmail.com>
2026-08-23 15:17:25 +0330

    fix(tooltip): place arrow on correct edge for rtl inline sides

--- packages/ui/src/components/tooltip.tsx
+++ packages/ui/src/components/tooltip.tsx
@@ -117,7 +117,7 @@ function TooltipContent({
         >
           {children}
           {showArrow && (
-            <TooltipPrimitive.Arrow className="z-50 size-2.5 translate-y-[calc(-50%-2px)] rotate-45 rounded-[2px] bg-foreground fill-foreground data-[side=bottom]:top-1 data-[side=inline-end]:top-1/2! data-[side=inline-end]:-left-1 data-[side=inline-end]:-translate-y-1/2 data-[side=inline-start]:top-1/2! data-[side=inline-start]:-right-1 data-[side=inline-start]:-translate-y-1/2 data-[side=left]:top-1/2! data-[side=left]:-right-1 data-[side=left]:-translate-y-1/2 data-[side=right]:top-1/2! data-[side=right]:-left-1 data-[side=right]:-translate-y-1/2 data-[side=top]:-bottom-2.5" />
+            <TooltipPrimitive.Arrow className="z-50 size-2.5 translate-y-[calc(-50%-2px)] rotate-45 rounded-[2px] bg-foreground fill-foreground data-[side=bottom]:top-1 data-[side=inline-end]:top-1/2! data-[side=inline-end]:-left-1 data-[side=inline-end]:-translate-y-1/2 data-[side=inline-start]:top-1/2! data-[side=inline-start]:-right-1 data-[side=inline-start]:-translate-y-1/2 data-[side=left]:top-1/2! data-[side=left]:-right-1 data-[side=left]:-translate-y-1/2 data-[side=right]:top-1/2! data-[side=right]:-left-1 data-[side=right]:-translate-y-1/2 data-[side=top]:-bottom-2.5 rtl:data-[side=inline-end]:-right-1 rtl:data-[side=inline-end]:left-auto rtl:data-[side=inline-start]:right-auto rtl:data-[side=inline-start]:-left-1" />
           )}
         </TooltipPrimitive.Popup>
       </TooltipPrimitive.Positioner>
